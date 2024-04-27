d9efdd8de1dadf1d26e2737bf73f7fbd0d267cae
ritu-477 <ritujoharbishnoi@gmail.com>
2024-04-27 11:23:38 +0530

    slider-card-done

--- assets/js/style.js
+++ assets/js/style.js
@@ -3,3 +3,55 @@ function openNav() {
     document.getElementById("navbar").classList.toggle("start-0");
     document.body.classList.toggle("overflowhidden")
 }
+
+
+
+$('.responsive').slick({
+    dots: true,
+    infinite: true,
+    speed: 200,
+    autoplay: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: ".prew-btn",
+    prevArrow: ".next-btn",
+    responsive: [
+        {
+            breakpoint: 1440,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 1400,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 1050,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: false,
+
+            }
+        },
+
+    ]
+});
+
